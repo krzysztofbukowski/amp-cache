23ad12f2cb04d6b01c0446487c9eefab3517ef8e
Krzysztof Bukowski <krzysztof@giffgaff.co.uk>
2017-07-05 15:05:26 +0100

    Fixed creating signature

--- run.sh
+++ run.sh
@@ -6,11 +6,14 @@ TIMESTAMP=123
 SIGNATURE=123
 CACHE_DOMAIN="cdn.ampproject.org"
 ACTION="flush"
-UPDATE_CACHE_REQUEST="/update-cache/c/s/www.giffgaff.com/$PAGE?amp_action=$ACTION&amp_ts=$TIMESTAMP"
+UPDATE_CACHE_REQUEST="/update-cache/c/s/www.giffgaff.com/amp/$PAGE?amp_action=$ACTION&amp_ts=$TIMESTAMP"
 
 CACHE_CLEAR_URL="https://www.giffgaf-com.$CACHE_DOMAIN$UPDATE_CACHE_REQUEST&amp_url_signature=$SIGNATURE"
 
-echo >$TMP_DIR/url.txt $UPDATE_CACHE_REQUEST cat url.txt | openssl dgst -sha256 -sign amp-private-key.pem >$TMP_DIR/signature.bin
+echo $CACHE_CLEAR_URL
+
+echo >$TMP_DIR/url.txt $UPDATE_CACHE_REQUEST
+cat $TMP_DIR/url.txt | openssl dgst -sha256 -sign amp-private-key.pem >$TMP_DIR/signature.bin
 
 echo "Verifing request signature"
 
