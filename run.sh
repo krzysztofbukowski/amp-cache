556d021fd1c87107a759a265680505d5f6aed87e
Krzysztof Bukowski <krzysztof@giffgaff.co.uk>
2017-07-05 10:44:34 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 
+TMP_DIR=".tmp"
+
 PAGE="sim-only-plans"
 TIMESTAMP=123
 SIGNATURE=123
 CACHE_DOMAIN="cdn.ampproject.org"
 ACTION="flush"
+UPDATE_CACHE_REQUEST="/update-cache/c/s/www.giffgaff.com/$PAGE?amp_action=$ACTION&amp_ts=$TIMESTAMP"
 
-CACHE_CLEAR_URL="https://www.giffgaf-com.$CACHE_DOMAIN/update-cache/c/s/www.giffgaff.com/$PAGE?amp_action=$ACTION&amp_ts=$TIMESTAMP&amp_url_signature=$SIGNATURE"
+CACHE_CLEAR_URL="https://www.giffgaf-com.$CACHE_DOMAIN$UPDATE_CACHE_REQUEST&amp_url_signature=$SIGNATURE"
 
-echo $CACHE_CLEAR_URL
+echo >$TMP_DIR/url.txt $UPDATE_CACHE_REQUEST cat url.txt | openssl dgst -sha256 -sign amp-private-key.pem >$TMP_DIR/signature.bin
+
+echo "Verifing request signature"
+
+openssl dgst -sha256 -signature $TMP_DIR/signature.bin -verify amp-public-key.pem $TMP_DIR/url.txt
