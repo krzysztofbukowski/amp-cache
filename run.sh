b5cd8b524706ac3fc15679c18c9f6bdadbf6e5cf
Krzysztof Bukowski <krzysztof@giffgaff.co.uk>
2017-07-06 12:08:51 +0100

    Added base64 signature to the request

--- run.sh
+++ run.sh
@@ -3,12 +3,9 @@ TMP_DIR=".tmp"
 
 PAGE="sim-only-plans"
 TIMESTAMP=`date +%s`
-SIGNATURE=123
 CACHE_DOMAIN="cdn.ampproject.org"
 ACTION="flush"
 UPDATE_CACHE_REQUEST="/update-cache/c/s/www.giffgaff.com/amp/$PAGE?amp_action=$ACTION&amp_ts=$TIMESTAMP"
-
-CACHE_CLEAR_URL="https://www-giffgaf-com.$CACHE_DOMAIN$UPDATE_CACHE_REQUEST&amp_url_signature=$SIGNATURE"
 
 echo $CACHE_CLEAR_URL
 
@@ -18,3 +15,12 @@ cat $TMP_DIR/url.txt | openssl dgst -sha256 -sign amp-private-key.pem >$TMP_DIR/
 echo "Verifing request signature"
 
 openssl dgst -sha256 -signature $TMP_DIR/signature.bin -verify amp-public-key.pem $TMP_DIR/url.txt
+
+if [[ $? -eq 0 ]]; then
+    SIGNATURE=`base64 $TMP_DIR/signature.bin`
+
+    CACHE_CLEAR_URL="https://www-giffgaf-com.$CACHE_DOMAIN$UPDATE_CACHE_REQUEST&amp_url_signature=$SIGNATURE"
+    echo $CACHE_CLEAR_URL
+
+    curl $CACHE_CLEAR_URL
+fi
